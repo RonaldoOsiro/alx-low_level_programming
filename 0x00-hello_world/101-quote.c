42ffbcad8724b4fa7a6cde552e7e2f07403659f0
RonaldoOsiro <ronaldorona722@gmail.com>
2023-03-19 11:55:15 -0700

    Unix print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - printing the exact word
- *
- * Return: 1 when successful
+ * main - Unix programe
+ * Return: 1 on success
  */
-
 int main(void)
-
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-}	
+}
