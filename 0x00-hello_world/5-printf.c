28fed37646e5ac38aef2f10219785b1bc8970f62
RonaldoOsiro <ronaldorona722@gmail.com>
2023-03-19 05:13:39 -0700

    Hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 0 in success
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
