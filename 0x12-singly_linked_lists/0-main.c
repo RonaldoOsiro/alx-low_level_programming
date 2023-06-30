fd68dea34593757221a77dd63a571212b4a0e79c
RonaldoOsiro <ronaldorona722@gmail.com>
2023-06-30 07:53:09 -0700

    Printing list

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	list_t *head;
 	list_t *new;
-	list_t hello = {"World", 5, NULL};
+	list_t hello = {"World", 5, NULL}
 	size_t n;
 
 	head = &hello;
@@ -37,5 +37,4 @@ int main(void)
 
 	free(new);
 	return (0);
-
 }
