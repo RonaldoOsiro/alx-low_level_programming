33aaedf391408b9f37a591cac569d7dabad3ad51
RonaldoOsiro <ronaldorona722@gmail.com>
2023-06-30 06:46:52 -0700

    Node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,6 +20,8 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (!new)
+	return (NULL);
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
