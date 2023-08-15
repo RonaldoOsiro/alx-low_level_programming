b2f76b3b2201ad44e5123059067e9f22f26913c7
RonaldoOsiro <ronaldorona722@gmail.com>
2023-08-15 12:28:03 -0700

    Dog poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,17 @@
 #define DOG_H
 
 /**
- * struct dog - dog as a basic info
- * @name: First info
- * @age: Second info
- * @owner: Third owner
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
  * Description: Longer description
  */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 
